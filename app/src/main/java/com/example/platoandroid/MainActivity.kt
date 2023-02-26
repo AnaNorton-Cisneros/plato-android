086a8a7d389704a53b8434d8498278597d061041
AnaNorton-Cisneros <ananortoncisneros@gmail.com>
2023-02-26 15:38:02 -0600

    main activity commit

--- app/src/main/java/com/example/platoandroid/MainActivity.kt
+++ app/src/main/java/com/example/platoandroid/MainActivity.kt
@@ -46,11 +46,11 @@ import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -100,7 +100,13 @@ class MainActivity : ComponentActivity() {
              * it's just so you can leave helpful notes like this one :)
              */
             // YOUR TEST AREA vvv
-
+            Text(text="HEllO WORLD!")
+            BottomAppBar {
+              IconButton(onClick = { /* doSomething() */ }) {
+                Icon(Icons.Filled.Menu, contentDescription = "Localized description")
+              }
+            }
+            TodoList(title = "My todo list")
             // ^^^^^ add above ^^^^^
             // ^^^ YOUR TEST AREA
           }
